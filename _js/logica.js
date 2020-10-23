7791a2008b710b1899645d37c628c279b41b4674
Marcelo Antonio <marceloa.mnzs@gmail.com>
2020-10-23 17:04:43 -0200

    Criação da verificação do lado inferior esquerdo e ajustes nas demais verificações em cáll com: Erickson, Cibele, Ian e Marcelo

--- _js/logica.js
+++ _js/logica.js
@@ -5,8 +5,8 @@ var jogadorAtual = "B";
 var oponente;
 var ultimaPosicaoValidaSupDir = "";
 var ultimaPosicaoValidaSupEsq = "";
-var ultimaPosicaoValidaInfDir = "";
 var ultimaPosicaoValidaInfEsq = "";
+var ultimaPosicaoValidaInfDir = "";
 
 
 var tabuleiro = [
@@ -21,21 +21,29 @@ var tabuleiro = [
 ]
 
 function getPeca(linha,coluna){
+    if(tabuleiro[linha][coluna] != jogadorAtual){
+        return;
+    }
     if(tabuleiro[linha][coluna] == "X"){
         return;
     }
     if(contador == 1){
-        if(tabuleiro[linha][coluna] == jogadorAtual){
-            document.getElementById(ultimaJogada).style.backgroundColor="initial";
+        if(tabuleiro[linha][coluna] == " "){
+            return;
+        } else if(tabuleiro[linha][coluna] == jogadorAtual){
             document.getElementById(linha + "-" + coluna).style.backgroundColor="red";
+            document.getElementById(ultimaJogada).style.backgroundColor="initial";
             document.getElementById(ultimaPosicaoValidaSupEsq).style.backgroundColor="initial";
             document.getElementById(ultimaPosicaoValidaSupDir).style.backgroundColor="initial";
+            document.getElementById(ultimaPosicaoValidaInfEsq).style.backgroundColor="initial";
+            //document.getElementById(ultimaPosicaoValidaInfDir).style.backgroundColor="initial";
             verificaSupEsquerdo(linha,coluna,jogadorAtual);
             verificaSupDireito(linha,coluna,jogadorAtual);
+            verificaInfEsquerdo(linha,coluna,jogadorAtual);
             ultimaJogada = linha + "-" + coluna;
         } else {
             contador = 0;
-        }
+        }    
     } else if(contador == 0){
         if(tabuleiro[linha][coluna] == " "){
             return;
@@ -43,18 +51,18 @@ function getPeca(linha,coluna){
         document.getElementById(linha + "-" + coluna).style.backgroundColor="red";
         verificaSupEsquerdo(linha,coluna,jogadorAtual);
         verificaSupDireito(linha,coluna,jogadorAtual);
+        verificaInfEsquerdo(linha,coluna,jogadorAtual);
         contador++
         ultimaJogada = linha + "-" + coluna;
     }
-
-    console.log(contador)
-    console.log(linha,coluna)
-
 }
 
 function verificaSupEsquerdo(linha,coluna,jogadorAtual){
     oponente = jogadorAtual == "B"? "P": "B";
     if(linha - 1 >= 0 && coluna - 1 >= 0){
+        if(tabuleiro[linha - 1][coluna - 1] == jogadorAtual){
+            ultimaPosicaoValidaSupEsq = (linha) + "-" + (coluna);
+        }
         if(tabuleiro[linha - 1][coluna - 1] == " "){
             document.getElementById((linha - 1) + "-" + (coluna - 1)).style.backgroundColor="#49cc37";
             ultimaPosicaoValidaSupEsq = (linha-1) + "-" + (coluna-1);
@@ -75,6 +83,10 @@ function verificaSupEsquerdo(linha,coluna,jogadorAtual){
 function verificaSupDireito(linha,coluna,jogadorAtual){
     oponente = jogadorAtual == "B"? "P": "B";
     if(linha - 1 >= 0 && coluna + 1 <= 7){
+        if(tabuleiro[linha - 1][coluna + 1]== jogadorAtual){
+            ultimaPosicaoValidaSupDir = (linha) + "-" + (coluna);
+            
+        }
         if(tabuleiro[linha - 1][coluna + 1] == " "){
             document.getElementById((linha - 1) + "-" + (coluna + 1)).style.backgroundColor="#49cc37";
             ultimaPosicaoValidaSupDir = (linha-1) + "-" + (coluna+1);
@@ -84,6 +96,30 @@ function verificaSupDireito(linha,coluna,jogadorAtual){
                 if(tabuleiro[linha - 2][coluna + 2] == " "){
                     document.getElementById((linha - 2) + "-" + (coluna + 2)).style.backgroundColor="#49cc37";
                     ultimaPosicaoValidaSupDir = (linha-1) + "-" + (coluna+1);
+                    return true;
+                }
+            }
+        }
+    } 
+    return false;
+}
+
+function verificaInfEsquerdo(linha,coluna,jogadorAtual){
+    oponente = jogadorAtual == "B"? "P": "B";
+    if(linha + 1 <= 7 && coluna - 1 >= 0){
+        if(tabuleiro[linha + 1][coluna - 1] == jogadorAtual){
+            ultimaPosicaoValidaInfEsq = (linha) + "-" + (coluna);
+ 
+        }
+        if(tabuleiro[linha + 1][coluna - 1] == " "){
+            document.getElementById((linha + 1) + "-" + (coluna - 1)).style.backgroundColor="#49cc37";
+            ultimaPosicaoValidaInfEsq = (linha) + "-" + (coluna);
+            return true;
+        } else if(tabuleiro[linha + 1][coluna - 1] == oponente){
+            if(linha + 2 <= 7 && coluna - 2 >= 0){
+                if(tabuleiro[linha + 2][coluna - 2] == " "){
+                    document.getElementById((linha + 2) + "-" + (coluna - 2)).style.backgroundColor="#49cc37";
+                    ultimaPosicaoValidaInfEsq = (linha+1) + "-" + (coluna-1);
                     return true;
                 }
             }
